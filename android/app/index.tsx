b99e8288189995f54997a8fd1b36119632939ed1
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-03-16 15:23:33 +0800

    COntinue

--- android/app/index.tsx
+++ android/app/index.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import "../global.css"
-const index = () => {
-  return (
-    <View>
-      <Text className='text-red-500 text-3xl'>index</Text>
-    </View>
-  )
-}
+import { View, Image, Dimensions, Text, TextInput } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import '../global.css'
+import Input from "@/components/input";
 
-export default index
+const { width, height } = Dimensions.get("window");
+
+const Index = () => {
+  return (
+    <View className="h-full w-full pt-52">
+      <StatusBar backgroundColor="white" style="inverted"></StatusBar>
+      <Image
+        className={'absolute w-full h-full'}
+        source={require("../assets/images/loginBackground.jpg")}
+        style={{ width: width, height: height + 50 }}
+      />
+
+      <View className="flex-row justify-around items-center absolute w-full">
+        <Image className="h-[225px] w-[90px]" 
+          resizeMode="contain"
+          source={require("../assets/images/light.png")}
+        />
+        <Image className="h-[155px] w-[40px]" 
+          resizeMode="contain"
+          source={require("../assets/images/light.png")}
+        />
+      </View>
+
+      <View className="pt-40 flex items-center px-5 ">
+        <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>
+          Login
+        </Text>
+        <Input />
+      </View>
+    </View>
+  );
+};
+
+export default Index;
---
+++ android/components/input.tsx
@@ -0,0 +1,14 @@
+import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+import { InputProps } from '@/utils/types'
+
+
+const Input = () => {
+  return (
+    <View className="p-5 bg-slate-200 w-full rounded-3xl border-2 border-gray-300">
+      <TextInput>Inputsdf sdf</TextInput>
+    </View>
+  )
+}
+
+export default Input
