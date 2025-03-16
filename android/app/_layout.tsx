485c492d01725c8920f25508ea6a09a33c799caf
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-03-16 14:16:24 +0800

    Continue

---
+++ android/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+
+const Layout = () => {
+  return <Stack screenOptions={{ headerShown: false }}></Stack>
+}
+
+export default Layout
---
+++ android/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css"
+const index = () => {
+  return (
+    <View>
+      <Text className='text-red-500 text-3xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
