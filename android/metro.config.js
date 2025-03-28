dd2c824f171163e2eb6f05fe1c9751c2f8b08b96
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-03-28 10:30:31 +0800

    react native wind configure

--- android/metro.config.js
+++ android/metro.config.js
@@ -1,6 +1,16 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require('nativewind/metro');
+
+// const config = getDefaultConfig(__dirname)
+// config.resolver.sourceExts.push('cjs');
+// module.exports = withNativeWind(config, { input: './global.css' })
+
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
-config.resolver.sourceExts.push('cjs');
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
+
 module.exports = withNativeWind(config, { input: './global.css' })
