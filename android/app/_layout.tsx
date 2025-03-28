cdf061620f25e1fa20c7d0ca3edc98b0259d80c5
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-03-28 10:30:31 +0800

    react native wind configure

--- android/app/_layout.tsx
+++ android/app/_layout.tsx
@@ -4,10 +4,12 @@ import React from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AuthProvider } from "../context/auth"
 import { StatusBar } from "react-native"
+import {verifyInstallation} from "nativewind"
 
 const client = new QueryClient()
 
 const RootLayout = () => {
+  verifyInstallation()
   return (
     <QueryClientProvider client={client}>
       <AuthProvider>
