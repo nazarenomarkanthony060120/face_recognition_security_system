82b45159f442014d711032e4efcc89ee9ac934e2
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-04-04 17:30:31 +0800

    Use Tanstack React Query

--- android/app/index.tsx
+++ android/app/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react"
 import { useRouter } from "expo-router"
 import { AuthContext } from "@/context/auth"
 import LoginScreen from "./screens/login"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const index = () => {
   const { user } = useContext(AuthContext) 
@@ -13,7 +14,12 @@ const index = () => {
     }
   }, [user, router])
 
-  return <LoginScreen />
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoginScreen />
+    </QueryClientProvider>
+  )
 }
 
 export default index
