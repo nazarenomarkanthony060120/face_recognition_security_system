dcf940f09ad1c4ef5fe303a6f8cd19eff172910f
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-04-16 11:15:25 +0800

    Continue

--- android/features/login/components/LoginFormCard.tsx
+++ android/features/login/components/LoginFormCard.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 import { useLogin } from '@/hooks/login'
 import { LoginRequest } from '@/utils/types'
-import { useRouter } from 'expo-router'
 import LoginFormHeader from './LoginFormHeader'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import LoginFormContents from './LoginFormContents'
@@ -10,19 +9,18 @@ import LoginFormFooter from './LoginFormFooter'
 import { ScrollView } from 'react-native'
 import Typo from '@/components/typo'
 import { getUserRoutes } from '@/features/common/part/getUserRoutes'
+import { useRouter } from 'expo-router'
 
 const LoginController = () => {
   const { control, handleSubmit } = useForm()
   const { mutate: login, error, isPending } = useLogin()
   const router = useRouter()
 
-  if (!router) return
-
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     login(data as LoginRequest, {
       onSuccess: (data) => {
         const route = getUserRoutes(data?.type)
-        router.replace()
+        router.replace(route)
       },
     })
   }
