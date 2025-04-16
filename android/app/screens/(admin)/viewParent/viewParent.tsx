f809d356824f822d6e65f2baf3161d07344b6662
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-04-16 17:13:48 +0800

    Continue

--- android/app/screens/(admin)/viewParent/viewParent.tsx
+++ android/app/screens/(admin)/viewParent/viewParent.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const viewParent = () => {
+  const { id } = useLocalSearchParams()
   return (
     <View>
-      <Text>viewParent</Text>
+      <Text>{id} ohyeah</Text>
     </View>
   )
 }
