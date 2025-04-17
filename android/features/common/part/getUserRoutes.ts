ae8f1e44fa80991ef5cdfb2a97eb01cd58c973a4
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-04-17 18:01:19 +0800

    Fix

--- android/features/common/part/getUserRoutes.ts
+++ android/features/common/part/getUserRoutes.ts
@@ -7,10 +7,10 @@ type getUserRoutes = {
 export const getUserRoutes = (type: UserType) => {
   switch (type) {
     case UserType.USER:
-      return '/screens/(user)/dashboard'
+      return '/screens/(user)/dashboard/dashboard'
     case UserType.ADMIN:
       return '/screens/(admin)/dashboard/dashboard'
     default:
-      return '/screens/(user)/dashboard'
+      return '/screens/(user)/dashboard/dashboard'
   }
 }
